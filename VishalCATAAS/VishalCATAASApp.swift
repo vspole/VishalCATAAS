385996d46af3961b132b5526f34100be3dfdd4ab
vspole <vishal.polepalli@gmail.com>
2022-06-30 10:46:25 -0500

    Initial Commit

---
+++ VishalCATAAS/VishalCATAASApp.swift
@@ -0,0 +1,17 @@
+//
+//  VishalCATAASApp.swift
+//  VishalCATAAS
+//
+//  Created by Vishal Polepalli on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct VishalCATAASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
